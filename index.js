b84c1618dc5cf2cc4d877cabded64dc9a44dfe3c
Lewis <lewismunyambu@gmail.com>
2022-08-24 14:02:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+   // (input.addEventListener.called).to.be.true;
+   // binds an event listener in addingEventListener
+    let buttons = document.getElementById
+    ('button')
+{
+        buttons.addEventListener('click',()=> alert('i have been clicked'))
+    }
 }
+addingEventListener()
